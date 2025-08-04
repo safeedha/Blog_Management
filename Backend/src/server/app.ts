12d8231930d711ca79b077bdb71ac05972245e1a
safeedha <mksafeedha@gmail.com>
2025-08-04 16:03:11 +0530

    add cores path

--- Backend/src/server/app.ts
+++ Backend/src/server/app.ts
@@ -12,7 +12,7 @@ export class App{
     this.app=express()
     this.app.use(morgan('dev'))
       this.app.use(cors({
-      origin: ['http://localhost:5173','https://medimate-rust.vercel.app','https://www.medi-mate.safeedha.site'],
+      origin: ['http://localhost:5173','https://blog-management-gamma-ruby.vercel.app'],
       credentials: true
     }));
     this.app.use(express.json());
