6bc2c1c9f53f806ff1cafb4e39ade2bfdfa44433
safeedha <mksafeedha@gmail.com>
2025-08-21 14:07:43 +0530

    lazy component added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,51 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import { Suspense, lazy } from "react";
+import './App.css'
 
+// Normal imports
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Landing from './pages/Landing'
-import HomePage from './pages/Homepage'
 import CreateBlog from './pages/CreateBlog'
-import Profile from './pages/Profile'
 
 import Loginprotect from './component/Loginprotect'
 import UserProtect from './component/UserProtect'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer} from 'react-toastify';
-import './App.css'
+// Lazy imports
+const HomePage = lazy(() => import('./pages/Homepage'));
+const Profile = lazy(() => import('./pages/Profile'));
 
 function App() {
-
-
   return (
     <>
-        <BrowserRouter>
+      <BrowserRouter>
         <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-    
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
         />
-            <Routes>
-              <Route path="/" element={<Landing />} />
-               <Route path="/login" element={<Loginprotect><Login /></Loginprotect>} />
-              <Route path="/signup" element={<Loginprotect><Signup /></Loginprotect>} />
-               <Route path="/home" element={<UserProtect><HomePage /></UserProtect>} />
-                <Route path="/create" element={<UserProtect><CreateBlog/></UserProtect>} />
-                <Route path="/profile" element={<UserProtect><Profile/></UserProtect>}/>
-            </Routes>
-        </BrowserRouter>
+  
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Loginprotect><Login /></Loginprotect>} />
+            <Route path="/signup" element={<Loginprotect><Signup /></Loginprotect>} />
+            <Route path="/home" element={<UserProtect><HomePage /></UserProtect>} />
+            <Route path="/create" element={<UserProtect><CreateBlog/></UserProtect>} />
+            <Route path="/profile" element={<UserProtect><Profile /></UserProtect>} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
